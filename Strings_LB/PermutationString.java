fcbe060eae542e4575a834f5e5e01fb954e8ffb9
shashwat-naik <96822149+shashwat-naik@users.noreply.github.com>
2024-07-24 00:35:27 +0530

    Eighted day

--- Strings_LB/PermutationString.java
+++ Strings_LB/PermutationString.java
@@ -1,13 +1,21 @@
 // Approach: Sort the string first by converting it into character array and then count its permutation
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PermutationString {
+    public static String sorting(String str) {
+        char temp[] = str.toCharArray();
+        Arrays.sort(temp);
+        return new String(temp);
+    }
 
     public static void permutation(String str, String permute, ArrayList<String> arr) {
         if (str.length() == 0) {
-            arr.add(permute);
+            if (!arr.contains(permute)) {
+                arr.add(permute);
+            }
             return;
         }
         for (int i = 0; i < str.length(); i++) {
@@ -19,6 +27,7 @@ public class PermutationString {
 
     public static List<String> findPermutation(String s) {
         ArrayList<String> arr = new ArrayList<>();
+        s=sorting(s);
         permutation(s, "", arr);
         return arr;
     }
